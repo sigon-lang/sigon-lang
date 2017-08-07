feaf1b6911482b00ec3cef9ad2af90e1dd562a18
Valdir <arnholdvaldir@gmail.com>
2017-08-07 10:28:09 -0300

    leitura do arquivo

--- src/main/java/br/ufsc/ine/antlr4/agent/Main.java
+++ src/main/java/br/ufsc/ine/antlr4/agent/Main.java
@@ -1,13 +1,10 @@
 package br.ufsc.ine.antlr4.agent;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.antlr.v4.runtime.ANTLRFileStream;
-import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
@@ -17,29 +14,28 @@ import agent.AgentParser;
 
 public class Main {
 
-	private static final String TEST = "beliefs(prop): a. a :- b.";
-
 	public static void main(String[] args) {
 
 		String filename = getLastArgument(args);
+
 		File file = new File(filename);
-
-		try (FileInputStream fis = new FileInputStream(filename)) {
-
-		} catch (FileNotFoundException e) {
+		if (!file.exists()) {
 			System.out.println("File " + filename + " not found.");
+			return;
+		}
+
+		try {
+			CharStream stream = CharStreams.fromFileName(filename);
+			AgentLexer lexer = new AgentLexer(stream);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			AgentParser parser = new AgentParser(tokens);
+			ParseTree tree = parser.agent();
+
+			ParseTreeWalker walker = new ParseTreeWalker();
+			walker.walk(new AgentWalker(), tree);
 		} catch (IOException e) {
 			System.out.println("I/O exception.");
 		}
-
-		CharStream stream = new ANTLRInputStream(TEST);
-		AgentLexer lexer = new AgentLexer(stream);
-		CommonTokenStream tokens = new CommonTokenStream(lexer);
-		AgentParser parser = new AgentParser(tokens);
-		ParseTree tree = parser.agent();
-
-		ParseTreeWalker walker = new ParseTreeWalker();
-		walker.walk(new AgentWalker(), tree);
 
 	}
 
