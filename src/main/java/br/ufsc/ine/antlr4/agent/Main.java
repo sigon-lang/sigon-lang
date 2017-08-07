5345c6e4b6a6ec55fdfd7170ec2320a2fd597f89
Valdir <arnholdvaldir@gmail.com>
2017-08-07 09:25:02 -0300

    iniciar leitura de arquivo

--- src/main/java/br/ufsc/ine/antlr4/agent/Main.java
+++ src/main/java/br/ufsc/ine/antlr4/agent/Main.java
@@ -1,5 +1,11 @@
 package br.ufsc.ine.antlr4.agent;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
@@ -16,6 +22,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		String filename = getLastArgument(args);
+		File file = new File(filename);
+
+		try (FileInputStream fis = new FileInputStream(filename)) {
+
+		} catch (FileNotFoundException e) {
+			System.out.println("File " + filename + " not found.");
+		} catch (IOException e) {
+			System.out.println("I/O exception.");
+		}
 
 		CharStream stream = new ANTLRInputStream(TEST);
 		AgentLexer lexer = new AgentLexer(stream);
