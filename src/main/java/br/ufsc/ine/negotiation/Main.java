a12f6ca13f9789270ebf14b5f8f8c0fc62aa9e1a
Rodrigo Notebook <pmello.rodrigor@gmail.com>
2018-11-14 16:01:31 -0200

    proposta para alpha usando AAT

--- src/main/java/br/ufsc/ine/negotiation/Main.java
+++ src/main/java/br/ufsc/ine/negotiation/Main.java
@@ -52,7 +52,9 @@ public class Main{
 	        //File agentFile = new File("/home/rodrigor/sigon-lang/src/main/java/br/ufsc/ine/negotiation/negotiation_aat.on");
 	        //File agentFile = new File("/home/rr/sigon-lang/src/main/java/br/ufsc/ine/negotiation/negotiation.on");
 	    	//File agentFile = new File("/home/rr/sigon-lang/src/main/java/br/ufsc/ine/negotiation/negotiation_aat.on");
-	    	File agentFile = new File("/home/rr/sigon-lang/src/main/java/br/ufsc/ine/negotiation/cenario2_aat.on");
+	    	//File agentFile = new File("/home/rr/sigon-lang/src/main/java/br/ufsc/ine/negotiation/cenario2_aat.on");
+	    	File agentFile = new File("/home/rr/sigon-lang/src/main/java/br/ufsc/ine/negotiation/cenario2_aatV2.on");
+
 	        CharStream stream = CharStreams.fromFileName(agentFile.getAbsolutePath());
 	        AgentLexer lexer = new AgentLexer(stream);
 	        CommonTokenStream tokens = new CommonTokenStream(lexer);
